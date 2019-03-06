b031d142fcc35d8990636d57ed2ce8903b62b5cc
Javier Calatrava <javier.calatravallaveria@altran.com>
2019-03-06 19:48:01 +0100

    Clean base project and basic pod library installation

---
+++ Podfile
@@ -0,0 +1,28 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'wgs' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for wgs
+  pod 'R.swift'
+
+  # Pods for Networking
+  pod 'Alamofire'
+
+  # Data Persistance
+   pod 'RealmSwift', '3.13.1'
+
+  # Pods for UI
+  pod 'SDWebImage'
+
+  # Pods logging
+  pod 'CocoaLumberjack/Swift', '~> 3.2.0'
+
+  target 'wgsTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ wgsTests/wgsTests.swift
@@ -0,0 +1,34 @@
+//
+//  wgsTests.swift
+//  wgsTests
+//
+//  Created by 08APO0516 on 06/03/2019.
+//  Copyright © 2019 jca. All rights reserved.
+//
+
+import XCTest
+@testable import wgs
+
+class wgsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
